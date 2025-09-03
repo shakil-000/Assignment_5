b9e70d113b84689d7f3046ad64658a9bbb7b1251
shakil-000 <shakilshahariarifti2k@gmail.com>
2025-09-03 17:20:58 +0600

    Call Buttons & History Related works done

--- script.js
+++ script.js
@@ -1,10 +1,75 @@
-const favCount = document.querySelector('#favCount');
+const LoveLogoCount = document.querySelector('#LoveCount');
 
 const heartIcons = document.querySelectorAll(".love-icon");
 
 for(const icon of heartIcons){
     icon.addEventListener('click', function(){
 
-        favCount.textContent = parseInt(favCount.textContent) + 1;
+        let CountingNumber = parseInt(LoveLogoCount.innerText);
+        CountingNumber++;
+        LoveLogoCount.innerText = CountingNumber;
+    }) 
+}
+
+
+//calling related 
+const NumberOfCoins = document.getElementById('coinCount')
+const callButtons = document.getElementsByClassName('call-btn')
+
+const historyList = document.getElementById('historyListing')
+
+for(const btn of callButtons){
+    btn.addEventListener('click', function(){
+
+        const card = btn.closest('.card');
+        const serviceName = card.querySelector('.card-name').innerText
+        const serviceNumber = card.querySelector('.card-number').innerText
+        
+        let UpdatedCoins = parseInt(NumberOfCoins.innerText);
+
+        if(UpdatedCoins < 20){
+            alert("Not enough coins to make a call!");
+            return;
+        }
+        else{
+            alert(`Calling ${serviceName} (${serviceNumber})`);
+            NumberOfCoins.innerText = UpdatedCoins - 20; 
+            
+            //history related
+            const li = document.createElement('li')
+            const PresentTime = new Date();
+            const LocalTime = PresentTime.toLocaleTimeString();
+
+            li.innerHTML = `<div class = "flex justify-between items-center bg-gray-200 p-2 rounded-md">
+            <div>
+             <p class = "font-medium">${serviceName}</p>
+             <p class = "text-gray-500 text-sm">${serviceNumber}</p>
+             </div>
+             <div class = "text-gray-500 text-sm">${LocalTime}</div>
+             </div> `;
+
+            li.classList.add('mb-2')
+            historyList.appendChild(li);   
+            
+        }
+
+
     })
 }
+
+
+  //clear history related
+document.getElementById('clearHistory')
+.addEventListener('click', function(){
+
+    historyList.style.display = 'none' ;
+})
+
+
+
+
+
+
+
+
+
